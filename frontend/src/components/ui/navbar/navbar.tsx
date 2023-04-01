acdf6f1a87ed939e2d1f3fd068b87010d1524742
try things <trythings3@gmail.com>
2023-04-01 20:39:55 +0300

    Gil: nav bar

--- frontend/src/components/ui/navbar/navbar.tsx
+++ frontend/src/components/ui/navbar/navbar.tsx
@@ -1,29 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { BsCart2 } from 'react-icons/bs';
 import styles from './navbar.module.css';
 
 export default function NavBar() {
+  const location = useLocation();
+  const routes = {
+    Packages: '/packages',
+    Plan_a_trip: '/plan_a_trip',
+    Travelers_community: '/community',
+    Sign_in: '/sign_in',
+  };
   return (
-    <ul>
-      <li>
-        <Link to='/'>
-          Trave<span>log</span>
-        </Link>
-      </li>
-      <li>
-        <Link to='/packages'>Packages</Link>
-      </li>
-      <li>
-        <Link to='/plan_a_trip'>Plan a trip</Link>
-      </li>
-      <li>
-        <Link to='/community'>Travellers community</Link>
-      </li>
-      <li>
-        <Link to='/sign_out'>Sign out</Link>
-      </li>
-      <li>
-        <Link to='/cart'>cart</Link>
-      </li>
+    <ul className={styles.navbar_container}>
+      <span>
+        <li>
+          <Link className={styles.navbar_item} to='/'>
+            Trave<span className={styles.title_log}>Log</span>
+          </Link>
+        </li>
+      </span>
+      <span className={styles.navbar_pages}>
+        {Object.entries(routes).map((val) => (
+          <li key={`route_${val[0]}`}>
+            <Link to={val[1]} className={`${styles.navbar_item} ${location.pathname === val[1] ? styles.selected_nav : ''}`}>
+              {val[0].replace(/_/g, ' ')}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <Link to='/cart' className={styles.navbar_item}>
+            <BsCart2 />
+          </Link>
+        </li>
+      </span>
     </ul>
   );
 }
