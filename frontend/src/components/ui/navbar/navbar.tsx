cd7f518e2e70a29262786a534dba71ae7e98eb8b
try things <trythings3@gmail.com>
2023-04-01 15:42:17 +0300

    gil: routes

---
+++ frontend/src/components/ui/navbar/navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import styles from './navbar.module.css';
+
+export default function NavBar() {
+  return (
+    <ul>
+      <li>
+        <Link to='/'>
+          Trave<span>log</span>
+        </Link>
+      </li>
+      <li>
+        <Link to='/packages'>Packages</Link>
+      </li>
+      <li>
+        <Link to='/plan_a_trip'>Plan a trip</Link>
+      </li>
+      <li>
+        <Link to='/community'>Travellers community</Link>
+      </li>
+      <li>
+        <Link to='/sign_out'>Sign out</Link>
+      </li>
+      <li>
+        <Link to='/cart'>cart</Link>
+      </li>
+    </ul>
+  );
+}
