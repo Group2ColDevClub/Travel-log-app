3a0cc95a5c2faaab914f394880d14b5399645891
Jonathanhammer <yonatanhmr@gmail.com>
2023-04-02 17:07:00 +0300

    css and Textarea component fixes

--- frontend/src/components/ui/Textarea/Textarea.jsx
+++ frontend/src/components/ui/Textarea/Textarea.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import propTypes from "prop-types";
 import "./Textarea.css";
+import "Travel-log-app/frontend/src/index.css";
 
 function Textarea(props){
-    const  {label, onChange, size, placeholder, className, name} = props;
+    const  {value, label, onChange, size, placeholder, className, name} = props;
 
-    const TextareaClasses = `textarea ${size ? `textarea--${size}` : ""}`;
+    const TextareaClasses = `textarea ${size ? `textarea--${size}` : "m"}`;
     return(
-        <div>
-            <label>{label}</label>
-            <Textarea 
+        <>
+            {label !== undefined && <label htmlFor="{name}">{label}</label>}
+            <textarea 
                 onChange={onChange}
+                value={value}
                 placeholder={placeholder}
                 className={`${TextareaClasses} ${className}`}
                 name={name}
             />
-        </div>
+        </>
     );
 } 
 
 Textarea.propTypes = {
-    label: propTypes.string.isRequired,
-    onChange: propTypes.func.isRequired,
+    label: propTypes.string,
+    onChange: propTypes.func,
     size: propTypes.oneOf(['s','m','l']),
-    placeholder: propTypes.string.isRequired,
-    className: propTypes.string.isRequired,
-    name: propTypes.string.isRequired
+    placeholder: propTypes.string,
+    className: propTypes.string,
+    name: propTypes.string
 };
 
-TextArea.defaultProps = {
-    size: '',
+Textarea.defaultProps = {
+    size: 'm',
   };
 export default Textarea;
