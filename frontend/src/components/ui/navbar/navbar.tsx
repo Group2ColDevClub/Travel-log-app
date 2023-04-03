b9cda98bc88e6aae41a1a6a417afe65c275c28d0
try things <trythings3@gmail.com>
2023-04-03 08:54:11 +0300

    Gil: cr fixes

--- frontend/src/components/ui/navbar/navbar.tsx
+++ frontend/src/components/ui/navbar/navbar.tsx
@@ -4,35 +4,33 @@ import styles from './navbar.module.css';
 
 export default function NavBar() {
   const location = useLocation();
-  const routes = {
-    Packages: '/packages',
-    Plan_a_trip: '/plan_a_trip',
-    Travelers_community: '/community',
-    Sign_in: '/sign_in',
-  };
+  const routes = [
+    { title: 'Packages', path: '/packages' },
+    { title: 'Plan a trip', path: '/plan_a_trip' },
+    { title: 'Travelers community', path: '/community' },
+    { title: 'Sign in', path: '/sign_in' },
+    { title: <BsCart2 />, path: '/cart' },
+  ];
   return (
-    <ul className={styles.navbar_container}>
-      <span>
-        <li>
-          <Link className={styles.navbar_item} to='/'>
-            Trave<span className={styles.title_log}>Log</span>
-          </Link>
-        </li>
-      </span>
-      <span className={styles.navbar_pages}>
-        {Object.entries(routes).map(([title, route]) => (
-          <li key={`route_${title}`}>
-            <Link to={route} className={`${styles.navbar_item} ${location.pathname === route ? styles.selected_nav : ''}`}>
-              {title.replace(/_/g, ' ')}
+    <nav>
+      <div className={styles.navbar_container}>
+        <span>
+          <div>
+            <Link className={styles.navbar_item} to='/'>
+              Trave<span className={styles.title_log}>Log</span>
             </Link>
-          </li>
-        ))}
-        <li>
-          <Link to='/cart' className={styles.navbar_item}>
-            <BsCart2 />
-          </Link>
-        </li>
-      </span>
-    </ul>
+          </div>
+        </span>
+        <span className={styles.navbar_pages}>
+          {routes.map(({ title, path }) => (
+            <div key={`route_${title}`}>
+              <Link to={path} className={`${styles.navbar_item} ${location.pathname === path ? styles.selected_nav : ''}`}>
+                {title}
+              </Link>
+            </div>
+          ))}
+        </span>
+      </div>
+    </nav>
   );
 }
