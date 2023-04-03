8631ddbc81b4646f02aed0e7a118a89bd82368a7
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-04-03 14:24:58 +0300

    Text component

---
+++ frontend/src/components/ui/Text/Text.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Text.module.css';
+
+function Text({ content, type, className }) {
+  return <p className={`${styles[type]} ${styles[className]}`}>{content}</p>;
+}
+
+Text.propTypes = {
+  content: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['Regular', 'Bold']),
+  className: PropTypes.string,
+};
+
+Text.defaultProps = {
+  type: 'Regular',
+};
+
+export default Text;
