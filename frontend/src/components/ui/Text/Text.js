b5f41c25accb796c3b5612322d0b2b6e3f4c7d39
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-04-03 17:36:52 +0300

    changes in Text component after cr

--- frontend/src/components/ui/Text/Text.js
+++ frontend/src/components/ui/Text/Text.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import styles from './Text.module.css';
 
 function Text({ content, type, className }) {
-  return <p className={`${styles[type]} ${styles[className]}`}>{content}</p>;
+  return <p className={`${styles[type]} ${className}`}>{content}</p>;
 }
 
 Text.propTypes = {
   content: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['Regular', 'Bold']),
-  className: undefined,
+  type: PropTypes.oneOf(['regular', 'bold']),
+  className: PropTypes.string,
 };
 
 Text.defaultProps = {
-  type: 'Regular',
+  type: 'regular',
 };
 
 export default Text;
