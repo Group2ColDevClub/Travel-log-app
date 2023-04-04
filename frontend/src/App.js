b3f5906e3c95737e63cd640de1d5ee0623e7a8fb
try things <trythings3@gmail.com>
2023-04-04 22:37:29 +0300

    Gil: cr - errorMsg, validationFunc, onSubmit, pass data and event on submit, Button text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Form from './components/ui/Form/Form.tsx';
 import NavBar from './components/ui/navbar/navbar.tsx';
 import { Packages, PlanTrip, SignIn, SignUp, CommunityPage, Cart } from './pages';
 
 export default function App() {
+  const inputs = [
+    {
+      label: 'name',
+      name: 'name',
+      placeholder: 'first name',
+      onChange: (value) => console.log(value),
+      validationFunc: (value) => console.log(value, 'hello', value === 'hello') || value === 'hello',
+      errorMsg: 'Error. not hello',
+    },
+    {
+      label: 'lastname',
+      name: 'lastname',
+      placeholder: 'last name',
+      onChange: (value) => console.log(value),
+      validationFunc: (value) => console.log(value, 'world', value === 'world') || value === 'world',
+      errorMsg: 'Error. not world',
+    },
+  ];
   return (
     <BrowserRouter>
       <NavBar />
+      <Form
+        inputs={inputs}
+        onSubmit={(data, e) => {
+          console.log(data);
+        }}
+      />
       <Routes>
         <Route path='/' element={<>/</>} />
         <Route path='/sign_in' element={<SignIn />} />
--- frontend/src/components/ui/Form/Form.tsx
+++ frontend/src/components/ui/Form/Form.tsx
@@ -1,46 +1,77 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import Button from '../Button/Button';
 import Input from '../Input/input';
 import styles from './Form.module.css';
 
 interface IFormInput {
   label: string;
+  name: string;
   placeholder?: string;
-  onChange: Function;
+  onChange: (value: any) => void;
   value?: string | number;
   defaultValue?: string | number;
+  validationFunc?: (value: any) => boolean;
+  errorMsg?: string;
 }
 
 interface IForm {
   inputs: Array<IFormInput>;
-  onSubmit: Function;
-  onScondaryClick?: Function;
+  onSubmit: (data: { [key: string]: any }, e: React.SyntheticEvent) => void;
   className?: string;
   primaryButtonText?: string;
+  onScondaryClick?: (e: React.SyntheticEvent) => void;
   secondaryButtonText?: string;
 }
 
-export default function Form({ inputs, onSubmit, onScondaryClick, className, primaryButtonText = 'Click', secondaryButtonText }: IForm) {
-  const handleSubmit = (e: React.SyntheticEvent) => {
+export default function Form({ inputs, onSubmit, onScondaryClick, className, primaryButtonText = 'Submit', secondaryButtonText }: IForm) {
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+  const getData = (formEvent: React.FormEvent<HTMLFormElement>) => {
+    const formData = new FormData(formEvent.currentTarget);
+    const data: { [key: string]: any } = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+    return data;
+  };
+
+  const validate = (data: { [key: string]: any }) => {
+    const allErrors: { [key: string]: string } = {};
+    inputs.forEach(({ label, validationFunc, errorMsg }) => {
+      if (!validationFunc(data[label])) {
+        allErrors[label] = errorMsg;
+      }
+    });
+    setErrors({ ...allErrors });
+    return !errors.length;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmit(e);
+    const data = getData(e);
+    if (validate(data)) {
+      onSubmit(data, e);
+    }
   };
 
   return (
-    <form className={[styles.form, className].join(' ')}>
-      {inputs.map(({ label, placeholder, onChange, value, defaultValue }) => (
-        <Input
-          key={label}
-          label={label}
-          name={label}
-          className={styles.form_item}
-          placeholder={placeholder}
-          onChange={(event) => onChange(event.target.value)}
-          value={value}
-          defaultValue={defaultValue}
-        />
+    <form className={[styles.form, className].join(' ')} onSubmit={handleSubmit}>
+      {inputs.map(({ label, name, placeholder, onChange, value, defaultValue, errorMsg }) => (
+        <span key={label}>
+          <Input
+            label={label}
+            name={name}
+            className={styles.form_item}
+            placeholder={placeholder}
+            onChange={(event) => onChange(event.target.value)}
+            value={value}
+            defaultValue={defaultValue}
+          />
+          {errors?.[label] && <span className={styles.input_error_msg}>{errorMsg}</span>}
+        </span>
       ))}
-      <Button type='submit' label={primaryButtonText} className={styles.form_item} onClick={handleSubmit} />
+      <Button type='submit' label={primaryButtonText} className={styles.form_item} />
       {secondaryButtonText && (
         <Button
           type='submit'
