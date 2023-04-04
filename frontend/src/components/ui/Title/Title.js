f3ee5d6d7497953008514007c28ab8fc42ee48cc
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-04-04 17:00:35 +0300

    changes after cr Title

--- frontend/src/components/ui/Title/Title.js
+++ frontend/src/components/ui/Title/Title.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import styles from './Title.module.css';
 
 function Title({ title, size, className, variant }) {
-  const classes = [styles.Title, styles[size], className].join(' ');
+  const classes = [styles.title, styles[size], className].join(' ');
   const TagName = variant;
   return <TagName className={classes}>{title}</TagName>;
 }
 
 Title.propTypes = {
   title: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(['first', 'second', 'third']),
+  size: PropTypes.oneOf(['s', 'm', 'l']),
   className: undefined,
-  variant: PropTypes.oneOf(['1', '2', '3', '4', '5', '6']).isRequired,
+  variant: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
 };
 
 Title.defaultProps = {
-  size: 'third',
+  size: 's',
 };
 
 export default Title;
