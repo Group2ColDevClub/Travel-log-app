98f8dfa1245d053ce8ecacf84aef68a28e264a9b
Jonathanhammer <yonatanhmr@gmail.com>
2023-04-18 17:09:01 +0300

    delete console order in Card

--- frontend/src/components/ui/Card/Card.js
+++ frontend/src/components/ui/Card/Card.js
@@ -5,8 +5,6 @@ function Card({ title, image, description, size, variant, className }) {
   const cardClassName = `card ${size && variant ? `card--${size}-${variant}` : `m-${variant}`}`;
   return (
     <div className={cardClassName}>
-      {/* eslint-disable-next-line no-console */}
-      {console.log(cardClassName)}
       {image && <img src={image} alt='backgroundImage' />}
       <div className='card-title'>
         <h3>{title}</h3>
