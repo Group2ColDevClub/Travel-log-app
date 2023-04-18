14cf31da112b700774bb97b01c4a349a920814c4
Jonathanhammer <yonatanhmr@gmail.com>
2023-04-18 18:11:48 +0300

    Community-page

--- frontend/src/pages/community/community.js
+++ frontend/src/pages/community/community.js
@@ -1,3 +1,97 @@
+import { AiFillLike } from 'react-icons/ai';
+import { MdAddComment } from 'react-icons/md';
+import Card from '../../components/ui/Card/Card.js';
+import Footer from '../../components/ui/Footer/Footer.js';
+import './community.css';
+
 export default function CommunityPage() {
-  return <div>CommunityPage</div>;
+  const travelersComments = [
+    {
+      id: 1,
+      title: 'Title-1',
+      image:
+        'https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      location: 'Tel-Aviv',
+      author: 'J.A',
+    },
+    {
+      id: 2,
+      title: 'Title-2',
+      image:
+        'https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      location: 'London',
+      author: 'A.G',
+    },
+    {
+      id: 3,
+      title: 'Title-3',
+      image:
+        'https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      location: 'USA',
+      author: 'F.T',
+    },
+    {
+      id: 4,
+      title: 'Title-4',
+      image:
+        'https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      location: 'Singapur',
+      author: 'T.P',
+    },
+    {
+      id: 5,
+      title: 'Title-5',
+      image:
+        'https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      location: 'Haifa',
+      author: 'A.G',
+    },
+    {
+      id: 6,
+      title: 'Title-2',
+      image:
+        'https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      location: 'Eilat',
+      author: 'A.G',
+    },
+  ];
+  return (
+    <div className='community-page'>
+      <div className='header-img'>
+        <img
+          className='community-header-image'
+          src='https://s3-alpha-sig.figma.com/img/5554/4fc2/ff571f96a8789d4fa4c5d8fc78995057?Expires=1681689600&Signature=ENvTf9A4fAzs78gUHQuJdy6pEz96pwrHTV53hJ564NuYGKH388p6r7ryQlFIg9eHRenVxybXYRsqzsCTs5YAsEXTZxqHzErPhzfpr9oNP8xXRcVC7O7cC1efUQ1imefkdw1c8r8NjSqaYd-IvhWzUwZdW5BGnxQod1E-A1uB18TQzo7pBh13HiK8Dmi6gxYFOH7Brj1d-AimS-Zf1Z-H6MIM9zX4OCJ2VNbYBuRjxJ7WYqURoAS-zT8jFaIYjklmMg2Z39ARnfDyUKm7EUAbjhwWICrzKX5iv6IOh9zK6Qf8rBduKZedGbb~aWR10iFcTay42h~-zisQyEv2hlvqhg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+          alt='viewImg'
+        />
+        <div className='title-block'>
+          <h1 className='community-title'>Travelers Community</h1>
+        </div>
+      </div>
+      <div className='community-cards'>
+        {travelersComments.map((cards) => (
+          <Card
+            key={cards.id}
+            title={cards.title}
+            image={cards.image}
+            description={
+              <div className='community-content'>
+                <h3>{cards.location}</h3>
+                <h5>{cards.author}</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel neque auctor, bibendum lectus id, lacinia turpis.</p>
+                <div className='community-buttons'>
+                  <p> 22,222</p>
+                  <AiFillLike className='community-like' fill='' size='1.6rem' />
+                  <p> 22,222 </p>
+                  <MdAddComment className='community-add-comment' fill='' size='1.6rem' />
+                </div>
+              </div>
+            }
+            size='m'
+            variant='wide'
+          />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 }
