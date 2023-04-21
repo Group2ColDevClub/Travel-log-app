41028c7db8c22a3d7fd80ca9eeedc14b58ba7c94
Jonathanhammer <yonatanhmr@gmail.com>
2023-04-21 15:27:11 +0300

    some changes in Card

--- frontend/src/components/ui/Card/Card.js
+++ frontend/src/components/ui/Card/Card.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import './Card.css';
 
-function Card({ title, image, description, size, variant, className }) {
+function Card({ title, image, description, size, variant, Footer, DescriptionHeader, className }) {
   const cardClassName = `card ${size && variant ? `card--${size}-${variant}` : `m-${variant}`}`;
   return (
     <div className={cardClassName}>
@@ -9,7 +9,11 @@ function Card({ title, image, description, size, variant, className }) {
       <div className='card-title'>
         <h3>{title}</h3>
       </div>
-      <div className='card-content'> {description} </div>
+      <div className='card-content'>
+        {DescriptionHeader && <div id='card-header-description'> {DescriptionHeader} </div>}
+        <div id='card-post-description'>{description}</div>
+        {Footer && <div id='card-footer'> {Footer} </div>}
+      </div>
     </div>
   );
 }
@@ -17,8 +21,10 @@ Card.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   size: PropTypes.oneOf(['s', 'm', 'l']).isRequired, // s=Cart,m=planTrip & Packages,l=Community
+  DescriptionHeader: PropTypes.node,
   variant: PropTypes.string,
   image: PropTypes.string,
+  Footer: PropTypes.node,
 };
 
 Card.defaultProps = {
