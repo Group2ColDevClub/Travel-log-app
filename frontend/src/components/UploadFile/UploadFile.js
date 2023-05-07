431b71bf02eab8f5544456caff6fb2e4eac4bb22
Jonathanhammer <yonatanhmr@gmail.com>
2023-05-07 16:08:20 +0300

    file upload for adding image to new post

---
+++ frontend/src/components/UploadFile/UploadFile.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { FaRegTrashAlt } from 'react-icons/fa';
+import styles from './UploadFile.module.css';
+
+function UploadFile() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imagePreview, setimagePreview] = useState(null);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setimagePreview(reader.result);
+      setIsImageLoaded(true);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const removeSelectedImage = () => {
+    setSelectedImage(null);
+    setimagePreview(null);
+    setIsImageLoaded(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={`${styles.file_input_container}`}>
+        {!isImageLoaded && (
+          // eslint-disable-next-line jsx-a11y/label-has-associated-control
+          <label htmlFor='imageInput' className={`${styles.file_input_label}`}>
+            <h3>Click to choose an image</h3>
+          </label>
+        )}
+        <input type='file' id='imageInput' onChange={handleImageChange} accept='image/*' style={{ display: 'none' }} />
+        {imagePreview && isImageLoaded && (
+          <div>
+            <img className={`${styles.image_upload}`} src={imagePreview} alt='Uploaded Preview' />
+            <FaRegTrashAlt className={`${styles.trash_button}`} type='button' onClick={removeSelectedImage} />
+          </div>
+        )}
+      </div>
+    </form>
+  );
+}
+export default UploadFile;
