679c0dbda08165943c30a68ff006f882011d9ad7
Jonathanhammer <yonatanhmr@gmail.com>
2023-05-09 19:12:06 +0300

    remove template literals

--- frontend/src/components/ModalNewComment/ModalNewComment.js
+++ frontend/src/components/ModalNewComment/ModalNewComment.js
@@ -1,25 +1,26 @@
 import styles from './ModalNewComment.module.css';
 import Input from '../ui/Input/input';
+import Text from '../ui/Text/Text';
 import Button from '../ui/Button/Button';
 
 function ModalNewComment({ title, post }) {
   return (
-    <div className={`${styles.comment_container}`}>
-      <h1 className={`${styles.comment_title}`}> {title} </h1>
-      <h4 className={`${styles.comment_subTitle}`}> {post.location} </h4>
-      <div className={`${styles.comment_image}`}>
-        <img className={`${styles.image}`} src={post.image} alt='post' />
+    <div className={styles.comment_container}>
+      <h2 className={styles.comment_title}>{title}</h2>
+      <Text className={styles.comment_subTitle} content={post.location} />
+      <div className={styles.comment_image}>
+        <img className={styles.image} src={post.image} alt='post' />
       </div>
-      <div className={`${styles.comment_body}`}>
-        <div className={`${styles.comments}`}>
-          <ul className={`${styles.list_item}`}>
+      <div className={styles.comment_body}>
+        <div className={styles.comments}>
+          <ul className={styles.list_item}>
             {post.comments.map((comment) => (
-              <li> {comment} </li>
+              <li>{comment}</li>
             ))}
           </ul>
         </div>
         <Input size='l' />
-        <Button className={`${styles.send_button}`} size='m' variant='primary' type='submit' label='Send' />
+        <Button className={styles.send_button} size='m' variant='primary' type='submit' label='Send' />
       </div>
     </div>
   );
